5431fc3abcbc3babdaf5c6a79ca735a07ff040c8
Kacper Dziubek <kacperdziubek@gmail.com>
2023-11-01 10:38:38 +0100

    Initial Commit

---
+++ UITesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UITesting
+//
+//  Created by Kacper Dziubek on 01/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UITesting/UITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingApp.swift
+//  UITesting
+//
+//  Created by Kacper Dziubek on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct UITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
